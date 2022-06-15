491ea7b9c633d4217e678e1837fea1be93b60a5e
naeemtawwos <naeem.mailto@gmail.com>
2022-06-15 10:34:50 +0530

    sort and find

--- js/array.js
+++ js/array.js
@@ -1,4 +1,5 @@
 const numbers = [43,53,63,73,83,93]
+const numbers_2 = [100,5,24,54,78,98]
 const caliphs = ['abu_bakr', 'umar', 'uthman', 'ali','hasan', 'muawiya']
 const caliph_2 = [
   'أبو بكر،',
@@ -42,3 +43,22 @@ caliphs.sort()
 console.log(caliphs)
 caliph_2.sort()
 console.log(caliph_2)
+// numbers_2.sort()
+console.log(numbers_2)
+
+val = numbers_2.sort(function(x,y){
+    return x - y
+})
+console.log(val)
+
+val = numbers_2.sort(function(x,y){
+    return  y - x
+})
+console.log(val)
+
+function under50(num){
+    return num < 50;
+}
+numbers_2.reverse()
+val = numbers_2.find(under50);
+console.log(val)
